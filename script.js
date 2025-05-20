47184f95b103c6cedd319b4e7fac8ff55d3b4a87
ktjung <55677285+ktjung@users.noreply.github.com>
2025-05-21 01:47:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ async function fetchBTCPrice() {
     localStorage.setItem("cached_btc_price", newBTCPrice);  // 비트코인 시세 저장
     localStorage.setItem("cached_btc_time", Date.now());   // 마지막 업데이트 시간 저장
 
-    // 시세를 자동으로 input 필드에 소수점 8자리까지 채움
+    // 시세를 자동으로 input 필드에 채움
     customInput.value = newBTCPrice.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     
     // 시세 반환
